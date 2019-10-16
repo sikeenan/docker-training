45c3ea6b720ecaa02517d5983a029b7a9b624a09
Simon Keenan <sikeenan@icloud.com>
2019-10-16 08:56:02 +0200

    added in the docker install script

---
+++ install-docker-ce.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Should be run as root. A quick and dirty install on Ubuntu 18.04
+
+apt-get update
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io
